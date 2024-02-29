88fe72405e45004482c39b4e5b9c2e51b65a9b0b
gitwes <wesleyfreire1707@gmail.com>
2024-02-28 22:13:26 -0300

    Montando logica

--- forca.py
+++ forca.py
@@ -7,7 +7,12 @@ while True:
     # criando a logica
     for letra in palavra:
         if letra in letras_usuarios:
- if ganhou:
+            print("letra")
+        else:
+            print("_")
+
+            
+if ganhou:
     print(f"parabens, voce ganhou. A palavra era: {palavra}")
 
 else:
